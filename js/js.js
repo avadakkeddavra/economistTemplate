2b31bb622bd1653444d2482b1228eb57c63330d1
avadakkeddavra <avadakkeddavra@gmail.com>
2017-04-26 09:34:01 +0300

    main page

---
+++ js/js.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#slider').slick({
+        infinite: true,
+        arrows : true,
+        
+    })
+    $('.slick-prev').html('<i class="fa fa-angle-left">');
+    $('.slick-next').html('<i class="fa fa-angle-right">');
+}) 
